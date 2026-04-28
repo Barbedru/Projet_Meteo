87707146c4ec91d7c2111b794ad6df7ae8cb9f64
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-04-28 15:48:50 +0200

    installation de timeago.js

--- premier_projet_vue/src/views/City.vue
+++ premier_projet_vue/src/views/City.vue
@@ -3,12 +3,19 @@
     <h1>{{ name }}</h1>
     <p>Météo : {{ weather }}</p>
     <p>Température : {{ temperature }} °C</p>
-    <p>Mise à jour : {{ updatedAt.toLocaleDateString() }}</p>
+    <p>Mise à jour : {{ formatDate(updatedAt) }}</p>
   </div>
 </template>
 
 <script>
+import { format, render, cancel, register } from 'timeago.js';
+
 export default {
-  props: ['name', 'weather', 'temperature', 'updateAt']
+  props: ['name', 'weather', 'temperature', 'updateAt'],
+  methods: {
+    formatDate(date) {
+      return format(date, 'fr')
+    }
+  }
 }
 </script>
