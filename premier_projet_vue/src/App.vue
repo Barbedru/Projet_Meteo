743ab5c486d914ec735583de58a87048079006ff
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-04-28 15:47:34 +0200

    ajout <br>

--- premier_projet_vue/src/App.vue
+++ premier_projet_vue/src/App.vue
@@ -1,9 +1,10 @@
-<script setup></script>
+
 
 <template>
   <main>
     <h1>Bienvenue sur l'application Météo</h1>
     <router-link to="/villes" >Voir la liste des villes</router-link>
+    <br>
 
     <router-link to="/ville">Voir une ville </router-link>
   <router-view></router-view>
