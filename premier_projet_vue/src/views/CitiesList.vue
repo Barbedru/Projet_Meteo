49cd1db5241930b4eb5951f5c3c698493d1de225
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-04-28 16:45:26 +0200

    Appel API externe

--- premier_projet_vue/src/views/CitiesList.vue
+++ premier_projet_vue/src/views/CitiesList.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Météo - Liste des villes</h1>
+    <p v-if="loading">Requête en cours...</p>
+    <p v-if="error" style="color: red">{{ error }}</p>
     <City
         v-for="city in cities"
         :key="city.id"
@@ -21,25 +23,34 @@ export default {
   components: {
     City
   },
-  data() {
+  data: function() {
     return {
-      cities: [
-        {
-          id: 1,
-          name: 'Ville 1',
-          weather: 'Ensoleillé',
-          temperature: 22.0,
-          updatedAt: new Date()
-        },
-        {
-          id: 2,
-          name: 'Ville 2',
-          weather: 'Peu nuageux',
-          temperature: 19.5,
-          updatedAt: new Date()
-        }
-      ]
+      Cities: [],
+      loading: false,
+      error: null,
+
     }
+  },
+
+  created() {            //le hook qui se déclenche quand le composant est créé
+    this.loading = true  //loading passe à true au début et revient à false une fois la requête terminée
+    fetch('https://api.openweathermap.org/data/2.5/find?lat=45.758&lon=4.765&cnt=20&cluster=yes&lang=fr&units=metric&APPID=4421eef49ebf0c1cafe95c95ae1ad098')
+        .then(response => response.json())
+        .then(data => {
+          this.cities = data.list.map(city => ({   // transforme les données de l'API au format attendu par le composant City
+            id: city.id,
+            name: city.name,
+            weather: city.weather[0].description,
+            temperature: city.main.temp,
+            updatedAt: new Date(city.dt * 1000)          //convertit le timestamp Unix en millisecondes
+          }))
+          this.loading = false
+        })
+        .catch(err => {
+          this.error = err.message
+          this.loading = false
+        })
   }
+
 }
 </script>
