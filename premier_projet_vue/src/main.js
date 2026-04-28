70804d28572970ac61fe39c8dd8bccae799350dc
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-04-28 15:19:31 +0200

    rectification

--- premier_projet_vue/src/main.js
+++ premier_projet_vue/src/main.js
@@ -1,15 +1,9 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import CitiesList from './views/CitiesList.vue'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: App },
-        { path: '/cities', component: CitiesList }
-    ]
-})
+import App from './App.vue'
+
+import router from './router/index.js'
+
 
 const app = createApp(App)
 app.use(router)
