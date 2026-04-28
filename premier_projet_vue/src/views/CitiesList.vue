4468cddae9d9fc73024a1330a3b12c287566cb6c
Barbedru <antoine.dru@le-campus-numerique.fr>
2026-04-28 15:48:12 +0200

    Création d'une liste

--- premier_projet_vue/src/views/CitiesList.vue
+++ premier_projet_vue/src/views/CitiesList.vue
@@ -1,5 +1,45 @@
 <template>
-
-<h1> Météo - Liste des villes</h1>
-
+  <div>
+    <h1>Météo - Liste des villes</h1>
+    <City
+        v-for="city in cities"
+        :key="city.id"
+        :name="city.name"
+        :weather="city.weather"
+        :temperature="city.temperature"
+        :updatedAt="city.updatedAt"
+    />
+  </div>
 </template>
+
+
+<script>
+
+import City from '../views/City.vue'
+
+export default {
+  components: {
+    City
+  },
+  data() {
+    return {
+      cities: [
+        {
+          id: 1,
+          name: 'Ville 1',
+          weather: 'Ensoleillé',
+          temperature: 22.0,
+          updatedAt: new Date()
+        },
+        {
+          id: 2,
+          name: 'Ville 2',
+          weather: 'Peu nuageux',
+          temperature: 19.5,
+          updatedAt: new Date()
+        }
+      ]
+    }
+  }
+}
+</script>
