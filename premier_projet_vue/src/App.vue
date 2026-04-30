1bd601b5e2b73da9f6e4ee9576d8e3b276c6739a
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-30 16:31:44 +0200

    .

--- premier_projet_vue/src/App.vue
+++ premier_projet_vue/src/App.vue
@@ -5,13 +5,13 @@
     <div class="banniere">
       <img src="https://static.vecteezy.com/ti/photos-gratuite/p2/5411405-meteo-previsions-icone-sur-smartphone-banniere-arriere-plan-rendu-3d-illustration-gratuit-photo.jpg" alt="bannière météo" />
     </div>
+
     <h1>Bienvenue sur l'application Météo</h1>
+
     <button><router-link to="/App" >Accueil</router-link></button>
     <br>
     <button><router-link to="/villes" >Voir la liste des villes</router-link></button>
     <br>
-<!--    <router-link to="/ville">Voir une ville </router-link>
-    <br>-->
     <button><router-link to="/carte">Map </router-link></button>
 
   <router-view></router-view>
@@ -24,6 +24,9 @@
 export default {
 
 }
+
+
+
 </script>
 
 <style scoped>
@@ -34,7 +37,8 @@ a {
 }
 
 main {
-  max-width: 800px;
+  max-width: 1000px;
+  border-radius : 10px;
   margin: 0 auto;
   text-align: center;
   padding: 2rem;
