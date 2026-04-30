0f741427551b872395172edb8d72b1c6cd286291
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-04-30 14:08:47 +0200

    correction

--- premier_projet_vue/src/views/City.vue
+++ premier_projet_vue/src/views/City.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>{{ name }}</h1>
+    <img :src="`https://openweathermap.org/img/wn/${icon}@2x.png`" :alt="weather" />
     <p>Météo : {{ weather }}</p>
     <p>Température : {{ temperature }} °C</p>
     <p>Mise à jour : {{ formatDate(updatedAt) }}</p>
@@ -10,8 +11,9 @@
 <script>
 import { format } from 'timeago.js';
 
+
 export default {
-  props: ['name', 'weather', 'temperature', 'updateAt'],
+  props: ['name', 'weather', 'temperature', 'updatedAt', 'icon', 'lat', 'lon'],
   methods: {
     formatDate(date) {
       return format(date, 'fr')
